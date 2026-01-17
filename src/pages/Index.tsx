d0df11e160a150d81f3c307deb6aab781c805de5
YUVRAJ JAISWAL <yuvrajjaiswal8568@gmail.com>
2026-01-17 23:38:24 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
@@ -9,6 +11,15 @@ import CallToAction from "@/components/CallToAction";
 import Footer from "@/components/Footer";
 
 const Index = () => {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    // If there is NO hash (like #features), scroll to top
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [hash]);
+
   return (
     <div className="min-h-screen">
       <Navigation />
